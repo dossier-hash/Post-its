e2ab3cf0c3f0635048e2e4bcdd28744aad855175
dossier-hash <dayy218@gmail.com>
2019-08-08 20:26:13 +0500

    Post form implemented

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Postit(db.Model):
+	"""The model of a post-it note"""
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(50))
+	#Can not be null/empty
+	content = db.Column(db.String(250), nullable=False)
+
+	def __repr__(self):
+		return f'<Post-it {self.id} created>'
+
+	def __str__(self):
+		return __repr__()
+
+@app.route('/', methods=['POST', 'GET'])
 def home():
-	return render_template('home.html') 
+	if request.method == 'POST':
+		post_content = request.form['content']
+		post_title = request.form['title']
+		new_post = Postit(content=post_content, title=post_title)
+
+		try: 
+			db.session.add(new_post)
+			db.session.commit()
+			flash('New post added')
+			return redirect('/')
+		except:
+			return redirect('/404')
+	else:
+		posts = Postit.query.order_by(Postit.id).all()
+		return render_template('home.html', posts=posts) 
 
 if __name__ == '__main__':
 	app.run(debug=True)
